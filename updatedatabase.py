328e7ec9e2ad1ab6bcc6ce55e2f63ed669300fc2
Rubar Gunduz <rubargunduz@gmail.com>
2025-05-13 10:44:09 +0300

    update database with updatedatabase.py

--- updatedatabase.py
+++ updatedatabase.py
@@ -3,17 +3,9 @@ import sqlite3
 conn = sqlite3.connect("users.db")
 cursor = conn.cursor()
 
-# Add 'email' if it doesn't exist
-try:
-    cursor.execute("ALTER TABLE customers ADD COLUMN email TEXT")
-except sqlite3.OperationalError:
-    print("Column 'email' already exists.")
-
-# Add 'phone' if it doesn't exist
-try:
-    cursor.execute("ALTER TABLE customers ADD COLUMN phone TEXT")
-except sqlite3.OperationalError:
-    print("Column 'phone' already exists.")
+cursor.execute("""
+DELETE FROM vehicles WHERE brand = "BMW"
+""")
 
 conn.commit()
 conn.close()
